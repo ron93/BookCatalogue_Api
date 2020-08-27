2a9f10a959b1cf1bf89df871e98b3614c881e99e
yagan0 <ryagan@kabarak.ac.ke>
2020-08-27 20:34:39 +0300

    dict_factory function

--- api.py
+++ api.py
@@ -3,41 +3,46 @@ from flask import Flask, jsonify,request
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-#test data for catalogue (list of dictionaries)
-books = [
-	{
-	'id' : 0 ,
-	'title' : 'A Fire Upon The deep',
-	'author' : 'Vernor Vinge',
-	'first_sentence' : 'The cold itself was dreamless.',
-	'published ' : '!992'
-	},
-	{
-	'id' : 1,
-	'title' : 'The ones who Walked Away From Omelas',
-	'author' : 'Ursula K. Le guin',
-	'first_sentence' : 'With a clamor of bells that set the swallows soaring , ...',
-	'published' : '1973'
-	},
-	{
-	'id' : 'Dhalgren',
-	'author' : 'Samuel R. Delany',
-	'first_sentence' : 'to wound the autumnal city.'
-	}
-
-]
+def dict_factory(cursor, row):
+	d = {}
+	for idx, col in enumerate(cursor.description):
+		d[col[0]] = row[idx]
+	return d
+	
 
 
 @app.route('/', methods=['GET'])
 def home():
 	return "<h1>Reading archive</h1><p>Prototype Api for reading science fiction novels </p>"
 
-	#route to return available entries in book catalogue
+#route to return available entries in book catalogue
 
 @app.route('/api/v1/resources/books/all', methods=['GET'])
 def api_all():
 	return jsonify(books)
 
+@app.route('/api/v1/resources/books', methods=['GET'])
+def api_id():
+	#checks if id is provided as part of the URL
+	#id id provided , assign to a variable
+	#if no id provided, display error in browser
+
+	if 'id' in request.args:
+		id = int(request.args['id'])
+
+	else:
+		return "Error : No id provided. please specify  book id"
+
+	#create empy list for result
+	results = []
+	#loop through data to find matching id provided
+
+	for book in books:
+		if book['id'] == id:
+			results.append(book)
+
+	#jsonify to convert python list dictionary to json format
+	return jsonify(results)
 
 
 if __name__=='__main__':
