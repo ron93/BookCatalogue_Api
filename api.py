dfa96c8f57f754c1dbcf235df1319933dbd8b1ad
yagan0 <ryagan@kabarak.ac.ke>
2020-08-28 12:02:57 +0300

    book filter -- api_filter function to filter by user supplied conditions

--- api.py
+++ api.py
@@ -31,30 +31,38 @@ def api_all():
 def page_not_found(e):
 	return "<h1> 404</h1><p>The resource could not be found.<p>",404
 
-
 @app.route('/api/v1/resources/books', methods=['GET'])
-def api_id():
-	#checks if id is provided as part of the URL
-	#id id provided , assign to a variable
-	#if no id provided, display error in browser
+def api_filter():
+    query_parameters = request.args
 
-	if 'id' in request.args:
-		id = int(request.args['id'])
+    id = query_parameters.get('id')
+    published = query_parameters.get('published')
+    author = query_parameters.get('author')
 
-	else:
-		return "Error : No id provided. please specify  book id"
+    query = "SELECT * FROM books WHERE"
+    to_filter = []
 
-	#create empy list for result
-	results = []
-	#loop through data to find matching id provided
+    if id:
+        query += ' id=? AND'
+        to_filter.append(id)
+    if published:
+        query += ' published=? AND'
+        to_filter.append(published)
+    if author:
+        query += ' author=? AND'
+        to_filter.append(author)
+    if not (id or published or author):
+        return page_not_found(404)
 
-	for book in books:
-		if book['id'] == id:
-			results.append(book)
+    query = query[:-4] + ';'
 
-	#jsonify to convert python list dictionary to json format
-	return jsonify(results)
+    conn = sqlite3.connect('books.db')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
 
+    results = cur.execute(query, to_filter).fetchall()
+
+    return jsonify(results)
 
 if __name__=='__main__':
 	app.run()
